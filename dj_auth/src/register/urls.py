9e4a0cb7c418177b3cfae7f321920cb5c021d308
PriyanshiChandra <chandrapriyanshi@gmail.com>
2021-07-24 18:57:04 +0530

    created views

---
+++ dj_auth/src/register/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from register import views
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
+
+
+]
