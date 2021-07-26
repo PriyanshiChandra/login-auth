31d124b7a46eeb161786ebe70da4cd800fcd2333
PriyanshiChandra <chandrapriyanshi@gmail.com>
2021-07-26 17:31:02 +0530

    final changes

--- dj_auth/src/register/urls.py
+++ dj_auth/src/register/urls.py
@@ -4,8 +4,7 @@ from register import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('login', views.login, name="login"),
-    path('logout', views.logout, name="logout"),
-
+    path('login', views.loginUser, name="login"),
+    path('logout', views.logoutUser, name="logout"),
 
 ]
--- dj_auth/src/register/views.py
+++ dj_auth/src/register/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate, login
 
+# test user cred- username: pc2810 password: $learndj$
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect ("/login")
     return render(request, 'index.html')
 
-def login(request):
+def loginUser(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print(username,password)
+        # check if user has entered correct credentials
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("/")
+        else:
+            return render(request, 'login.html')
     return render(request, 'login.html')
 
-def logout(request):
-    return render(request,'index.html')
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
